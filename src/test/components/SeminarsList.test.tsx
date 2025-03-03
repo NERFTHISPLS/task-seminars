15ace9b8d9e353397c07a1974fdd6c3d7c15d3dd
NERFTHIS <mustaevs@gmail.com>
2025-03-03 16:42:06 +0300

    test SeminarsList

--- src/test/components/SeminarsList.test.tsx
+++ src/test/components/SeminarsList.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 import * as useSeminars from '../../hooks/useSeminars';
 
@@ -8,13 +8,14 @@ import SeminarsList from '../../components/SeminarsList/SeminarsList';
 import type { Seminar } from '../../types';
 
 const useSeminarsSpy = jest.spyOn(useSeminars, 'useSeminars');
+const mockDeleteSeminar = jest.fn();
 
 test('shows loading indicator when fetching data', () => {
   useSeminarsSpy.mockReturnValue({
     isLoading: true,
     error: '',
     seminars: [],
-    deleteSeminar: () => {},
+    deleteSeminar: mockDeleteSeminar,
   });
 
   const { container } = render(<SeminarsList />);
@@ -27,7 +28,7 @@ test('shows error when fetch failed', () => {
     isLoading: false,
     error: 'Error occurred',
     seminars: [],
-    deleteSeminar: () => {},
+    deleteSeminar: mockDeleteSeminar,
   });
 
   const { container } = render(<SeminarsList />);
@@ -41,7 +42,7 @@ test('shows message when seminars list is empty', () => {
     isLoading: false,
     error: '',
     seminars: [],
-    deleteSeminar: () => {},
+    deleteSeminar: mockDeleteSeminar,
   });
 
   const { container } = render(<SeminarsList />);
@@ -73,7 +74,7 @@ test('shows seminars when loaded', () => {
     isLoading: false,
     error: '',
     seminars: seminarsMock,
-    deleteSeminar: () => {},
+    deleteSeminar: mockDeleteSeminar,
   });
 
   const { container } = render(<SeminarsList />);
@@ -81,4 +82,38 @@ test('shows seminars when loaded', () => {
   expect(container.querySelector('.loader')).not.toBeInTheDocument();
   expect(screen.getByText('Seminar 1')).toBeInTheDocument();
   expect(screen.getByText('Seminar 2')).toBeInTheDocument();
+});
+
+test('calls deleteSeminar when seminar is deleted', async () => {
+  const seminarsMock: Seminar[] = [
+    {
+      id: 1,
+      title: 'Seminar 1',
+      description: 'Description 1',
+      date: '02.03.2025',
+      time: '10:00',
+      photo: 'url 1',
+    },
+    {
+      id: 2,
+      title: 'Seminar 2',
+      description: 'Description 2',
+      date: '02.04.2025',
+      time: '09:00',
+      photo: 'url 2',
+    },
+  ];
+  useSeminarsSpy.mockReturnValue({
+    isLoading: false,
+    error: '',
+    seminars: seminarsMock,
+    deleteSeminar: mockDeleteSeminar,
+  });
+
+  render(<SeminarsList />);
+
+  fireEvent.click(screen.getAllByText('Удалить')[0]);
+  fireEvent.click(screen.getByText('Подтвердить'));
+
+  await waitFor(() => expect(mockDeleteSeminar).toHaveBeenCalledTimes(1));
 });
